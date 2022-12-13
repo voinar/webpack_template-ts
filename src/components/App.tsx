55ddcadbac5975dc28f285269f02e864b6b270d0
voinar <nifeprty@gmail.com>
2022-12-13 17:58:33 +0100

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,26 @@ export default function App() {
   return (
     <>
       <span>Hello.</span>
-      <h1> This is a Webpack + Typescript setup.</h1>
+      <h1> This is a Webpack + Typescript + React setup.</h1>
+      <ul>
+        dependencies:
+        <li> "@babel/core": "^7.20.5",</li>
+        <li> "react": "^18.2.0",</li>
+        <li> "react-dom": "^18.2.0",</li>
+        <li> "ts-loader": "^9.4.2",</li>
+        <li> "ts-node": "^10.9.1",</li>
+        <li> "typescript": "^4.9.4"</li>
+      </ul>
+      <ul>
+        devDependencies:
+        <li> "@types/react": "^18.0.26",</li>
+        <li> "@types/react-dom": "^18.0.9",</li>
+        <li> "@types/webpack-dev-server": "^4.7.2",</li>
+        <li> "html-webpack-plugin": "^5.5.0",</li>
+        <li> "tsconfig-paths-webpack-plugin": "^4.0.0",</li>
+        <li> "webpack-cli": "^5.0.1",</li>
+        <li>"webpack-dev-server": "^4.11.1"</li>
+      </ul>
     </>
   );
 }
